2cfd2682091b9c0af08652277d61116f4ee713de
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-06-13 11:43:28 +0100

    handled toggling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { Theme } from './models/theme.enum';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  theme!: Theme;
+  protected sub = new Subject<void>();
+
+  constructor(private themeService: ThemeService) { }
+
+  ngOnInit(): void {
+    this.themeService.currentTheme$.pipe(takeUntil(this.sub))
+    .subscribe(theme => this.theme = theme);
+  }
+
+  ngOnDestroy(): void {
+    this.sub.next();
+    this.sub.complete();
+  }
+
+  updateTheme(theme: Theme): void {
+    this.themeService.changeTheme(theme);
+  }
+}
