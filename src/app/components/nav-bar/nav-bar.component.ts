d364a94b1da70af30d186d5e7552da67624c438e
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-06-16 20:04:19 +0100

    made nav mobile responsive

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Subscription } from 'rxjs';
 import { Board } from 'src/app/models/board.model';
@@ -14,9 +15,13 @@ export class NavBarComponent implements OnInit, OnDestroy {
 
   @Input() opened!: boolean;
   @Input() activeBoard!: Board;
+  @Input() boards!: Board[];
   @Output() taskAdd = new EventEmitter<void>();
   @Output() boardEdit = new EventEmitter<void>();
   @Output() boardDelete = new EventEmitter<void>();
+  @Output() boardAdd = new EventEmitter<void>();
+  @Output() boardSelect = new EventEmitter<Board>();
+  sidebarShown = false;
   darkMode = false;
   protected sub = new Subscription();
 
@@ -38,6 +43,26 @@ export class NavBarComponent implements OnInit, OnDestroy {
   }
   deleteBoard(): void {
     this.boardDelete.emit();
+  }
+
+  addBoard(): void {
+    this.boardAdd.emit();
+  }
+
+  selectBoard(board: Board): void {
+    this.boardSelect.emit(board);
+  }
+
+  open(): void {
+    this.sidebarShown = true;
+  }
+
+  close(): void {
+    this.sidebarShown = false;
+  }
+
+  stop(e:Event) {
+    e.stopPropagation();
   }
 
 }
